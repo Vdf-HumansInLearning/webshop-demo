04c8d9243796b6fbec26cacf91bed556c1e4664a
Ionela <ionela.ciobanasu@vodafone.com>
2022-01-03 12:04:10 +0200

    work in progress, add middleware

--- webshop-app/app.js
+++ webshop-app/app.js
@@ -11,6 +11,7 @@ const cartRouter = require('./routes/cart');
 const authRouter = require('./routes/auth');
 const ordersRouter = require('./routes/orders');
 const dotenv = require('dotenv');
+const axios = require('axios').default;
 
 const app = express();
 dotenv.config();
@@ -19,11 +20,54 @@ dotenv.config();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middleware
+let getUser = function(req, res, next) {
+    console.log('LOGGED');
+
+    if (!req.url.startsWith('/stylesheets') && !req.url.startsWith('/javascripts')) {
+        let admin = false;
+        let loggedIn = false;
+        if (req.cookies.user_role === "admin") {
+            admin = true;
+        }
+
+        req.admin = admin;
+
+        if (req.cookies.user_role && req.cookies.user_id) {
+            loggedIn = true;
+        }
+
+        req.loggedIn = loggedIn;
+
+        if (loggedIn) {
+            axios.get(`http://localhost:3001/users/${req.cookies.user_id}`)
+                .then(function(response) {
+                    // handle success
+                    const user = response.data;
+                    req.user = user;
+                    console.log(user);
+                    next();
+                })
+                // .catch(function(error) {
+                //     // handle error
+                //     res.status(400).send("404 Not Found");
+                // });
+        }
+        // console.log('LOGGED');
+    }
+    console.log('LOGGED');
+    next();
+}
+
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(getUser);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -34,19 +78,20 @@ app.use('/auth', authRouter);
 app.use('/orders', ordersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
 
 module.exports = app;
--- webshop-app/routes/phones.js
+++ webshop-app/routes/phones.js
@@ -3,132 +3,125 @@ const axios = require('axios').default;
 const router = express.Router();
 
 /* GET Phones home page. */
-router.get('/', function (req, res, next) {
-  let admin = false;
-  let loggedIn = false;
-  if (req.cookies.user_role === "admin") {
-    admin = true;
-  }
-  if (req.cookies.user_role && req.cookies.user_id) {
-    loggedIn = true;
-  }
-  axios.get(`${process.env.API_HOST}:${process.env.API_PORT}/phones`, { params: req.query })
-    .then(function (response) {
-      // send phones and filters to render method
-      res.render('phones', {
-        title: 'Phones',
-        css: 'stylesheets/phones-style.css',
-        products: response.data.products,
-        filters: response.data.filters,
-        selectedFilters: response.data.selectedFilters,
-        admin: admin,
-        logged_in: loggedIn
-      });
-    })
-    .catch(function (error) {
-      // handle error
-      res.status(404).send("404 Not Found");
-    });
+router.get('/', function(req, res, next) {
+    axios.get(`${process.env.API_HOST}:${process.env.API_PORT}/phones`, { params: req.query })
+        .then(function(response) {
+            // send phones and filters to render method
+            res.render('phones', {
+                title: 'Phones',
+                css: 'stylesheets/phones-style.css',
+                products: response.data.products,
+                filters: response.data.filters,
+                selectedFilters: response.data.selectedFilters,
+                admin: req.admin,
+                logged_in: req.loggedIn,
+                username: req.user ? req.user.username : null
+            });
+        })
+        .catch(function(error) {
+            // handle error
+            res.status(404).send("404 Not Found");
+        });
 
 });
 
-router.post("/", function (req, res, next) {
-  axios.post(`${process.env.API_HOST}:${process.env.API_PORT}/phones`, {
-    name: req.body.name,
-    brand: req.body.brand,
-    operating_system: req.body.os,
-    price: Number(req.body.price),
-    discount: Number(req.body.discount),
-    quantity: Number(req.body.quantity),
-    availability_date: req.body.date,
-    rating: Number(req.body.rating),
-    image: req.body.imgUrl
-  }, {
-    "headers": {
-      "content-type": "application/json",
+router.post("/", function(req, res, next) {
+    axios.post(`${process.env.API_HOST}:${process.env.API_PORT}/phones`, {
+            name: req.body.name,
+            brand: req.body.brand,
+            operating_system: req.body.os,
+            price: Number(req.body.price),
+            discount: Number(req.body.discount),
+            quantity: Number(req.body.quantity),
+            availability_date: req.body.date,
+            rating: Number(req.body.rating),
+            image: req.body.imgUrl
+        }, {
+            "headers": {
+                "content-type": "application/json",
+            }
+        })
+        .then(function(response) {
+            res.status(200).send(`Adding phone ${req.body.name}`);
+        })
+        .catch(function(error) {
+            res.status(400).send({ message: "Bad request" });
+        });
+
+});
+
+router.get('/:phone', function(req, res, next) {
+    let admin = false;
+    let loggedIn = false;
+    if (req.cookies.user_role === "admin") {
+        admin = true;
     }
-  })
-    .then(function (response) {
-      res.status(200).send(`Adding phone ${req.body.name}`);
-    })
-    .catch(function (error) {
-      res.status(400).send({ message: "Bad request" });
-    });
-
-});
-
-router.get('/:phone', function (req, res, next) {
-  let admin = false;
-  let loggedIn = false;
-  if (req.cookies.user_role === "admin") {
-    admin = true;
-  }
-  if (req.cookies.user_role && req.cookies.user_id) {
-    loggedIn = true;
-  }
-
-  axios.get(`${process.env.API_HOST}:${process.env.API_PORT}/phones`)
-    .then(function (response) {
-      // handle success
-      let products = response.data.products;
-      let phone = products.find(item => item["name"] === req.params.phone);
-
-      let average = parseFloat(products.filter(product => product.brand === phone.brand && product.rating > 0).reduce((previous, current, index, array) => {
-        let calcSum = previous + current.rating;
-        if (index === array.length - 1) {
-          return calcSum / array.length;
-        }
-        return calcSum;
-      }, 0).toFixed(1));
-
-      res.render('details', {
-        title: 'Details',
-        css: 'stylesheets/details-style.css',
-        navHtml: '',
-        phone: phone,
-        average: average,
-        admin: admin,
-        logged_in: loggedIn
-      });
-    })
-    .catch(function (error) {
-      // handle error
-      res.status(404).send("404 Not Found");
-    });
-
-});
-
-router.put("/:id", function (req, res, next) {
-  axios.put(`${process.env.API_HOST}:${process.env.API_PORT}/phones/${req.params.id}`, {
-    name: req.body.name,
-    brand: req.body.brand,
-    operating_system: req.body.os,
-    price: Number(req.body.price),
-    discount: Number(req.body.discount),
-    quantity: Number(req.body.quantity),
-    availability_date: req.body.date,
-    rating: Number(req.body.rating),
-    image: req.body.imgUrl
-  }, {
-    "headers": {
-      "content-type": "application/json",
+    if (req.cookies.user_role && req.cookies.user_id) {
+        loggedIn = true;
     }
-  })
-    .then(function (response) {
-      res.status(200).send(`Updating phone ${req.body.name}`);
-    })
-    .catch(function (error) {
-      res.status(400).send({ message: "Bad request" });
-    });
+
+    axios.get(`${process.env.API_HOST}:${process.env.API_PORT}/phones`)
+        .then(function(response) {
+            // handle success
+            let products = response.data.products;
+            let phone = products.find(item => item["name"] === req.params.phone);
+
+            let average = parseFloat(products.filter(product => product.brand === phone.brand && product.rating > 0).reduce((previous, current, index, array) => {
+                let calcSum = previous + current.rating;
+                if (index === array.length - 1) {
+                    return calcSum / array.length;
+                }
+                return calcSum;
+            }, 0).toFixed(1));
+
+            res.render('details', {
+                title: 'Details',
+                css: 'stylesheets/details-style.css',
+                navHtml: '',
+                phone: phone,
+                average: average,
+                admin: admin,
+                logged_in: loggedIn
+            });
+        })
+        .catch(function(error) {
+            // handle error
+            res.status(404).send("404 Not Found");
+        });
 
 });
 
-router.delete('/:id', function (req, res) {
-  console.log(req.params.id);
-  res.send(`Deleting phone ${req.params.id}`);
-  axios.delete(`${process.env.API_HOST}:${process.env.API_PORT}/phones/${req.params.id}`, { data: req.params.id })
-    .then(() => console.log('Phone has been deleted'))
-    .catch(() => console.error('Failed to delete phone with id ' + req.params.id));
+router.put("/:id", function(req, res, next) {
+    axios.put(`${process.env.API_HOST}:${process.env.API_PORT}/phones/${req.params.id}`, {
+            name: req.body.name,
+            brand: req.body.brand,
+            operating_system: req.body.os,
+            price: Number(req.body.price),
+            discount: Number(req.body.discount),
+            quantity: Number(req.body.quantity),
+            availability_date: req.body.date,
+            rating: Number(req.body.rating),
+            image: req.body.imgUrl
+        }, {
+            "headers": {
+                "content-type": "application/json",
+            }
+        })
+        .then(function(response) {
+            res.status(200).send(`Updating phone ${req.body.name}`);
+        })
+        .catch(function(error) {
+            res.status(400).send({ message: "Bad request" });
+        });
+
+});
+
+router.delete('/:id', function(req, res) {
+    console.log(req.params.id);
+    res.send(`Deleting phone ${req.params.id}`);
+    axios.delete(`${process.env.API_HOST}:${process.env.API_PORT}/phones/${req.params.id}`, { data: req.params.id })
+        .then(() => console.log('Phone has been deleted'))
+        .catch(() => console.error('Failed to delete phone with id ' + req.params.id));
 });
 
 module.exports = router;
